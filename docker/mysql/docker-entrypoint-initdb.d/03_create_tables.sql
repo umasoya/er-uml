2098a2964a5f145247ca7840e06fb7765a2b11fc
umasoya <umasoya.0331@gmail.com>
2023-04-07 09:33:18 +0900

    add table in sample1 db

--- docker/mysql/docker-entrypoint-initdb.d/03_create_tables.sql
+++ docker/mysql/docker-entrypoint-initdb.d/03_create_tables.sql
@@ -16,3 +16,14 @@ CREATE TABLE sample1.post (
     PRIMARY KEY ( post_id )
 ) COMMENT '投稿';
 ALTER TABLE sample1.post ADD FOREIGN KEY fk_post__user(user_id) REFERENCES sample1.user(user_id);
+
+CREATE TABLE sample1.comment (
+    comment_id INT NOT NULL AUTO_INCREMENT COMMENT 'コメントID',
+    post_id INT NOT NULL COMMENT '投稿ID',
+    user_id INT NOT NULL COMMENT 'ユーザーID',
+    comment TEXT NOT NULL,
+
+    PRIMARY KEY ( comment_id )
+) COMMENT 'コメント';
+ALTER TABLE sample1.comment ADD FOREIGN KEY fk_comment__user(user_id) REFERENCES sample1.user(user_id);
+ALTER TABLE sample1.comment ADD FOREIGN KEY fk_comment__post(post_id) REFERENCES sample1.post(post_id);
